986af244d636445d1c5196c5afc06e80cc2e1210
김윤영 <pj.yyhi.kim@kalmate.net>
2023-11-24 11:27:40 +0900

    [ADD] stitches

--- app/components/treeItem.tsx
+++ app/components/treeItem.tsx
@@ -1,12 +1,33 @@
 import { TreeItemType } from '@/app/interfaces/treeItem';
 import Link from 'next/link';
+import { styled } from '@/stitches.config';
+
+const Text = styled('p', {
+  fontFamily: '$system',
+  color: '$hiContrast',
+
+  variants: {
+    size: {
+      1: {
+        fontSize: '$1',
+      },
+      2: {
+        fontSize: '$2',
+      },
+      3: {
+        fontSize: '$3',
+      },
+    },
+  },
+});
 
 const MenuButton = ({ tree, segment, handleClickTreeItem }: {
   tree: TreeItemType,
   segment: string,
   handleClickTreeItem: (slug: string) => void
 }) => (<Link href={`/${segment}/${tree.slug}`}>
-  <li style={{ color: '#ffffff' }} onClick={() => handleClickTreeItem(tree.slug)}>{tree.title}</li>
+  <li style={{ color: '#ffffff' }} onClick={() => handleClickTreeItem(tree.slug)}>
+    <Text size={3}>{tree.title}</Text></li>
 </Link>);
 
 const TreeItem = ({ tree, segment, handleClickTreeItem }: {
--- app/hr/components/header.tsx
+++ app/hr/components/header.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import Link from 'next/link';
 import { HR_SEGMENT } from '@/app/constants/constants';
+import { styled } from '@/stitches.config';
+
+const Text = styled('p', {
+  fontFamily: '$system',
+  color: '$hiContrast',
+  variants: {
+    size: {
+      1: {
+        fontSize: '$1',
+      },
+      2: {
+        fontSize: '$2',
+      },
+      3: {
+        fontSize: '$3',
+      },
+    },
+  },
+});
 
 const Header = () => {
   return (
     <div
       style={{ position: 'sticky', top: 0, width: '100%', height: '48px', minHeight: '48px', padding: '0 30px', borderBottom: '1px solid rgb(219, 219, 220)', backgroundColor: '#ffffff', display: 'flex', alignItems: 'center' }}>
-      <Link href={`/${HR_SEGMENT}`}>HR Header</Link>
+      <Link href={`/${HR_SEGMENT}`}><Text size={1}>HR Header</Text></Link>
     </div>
   );
 };
